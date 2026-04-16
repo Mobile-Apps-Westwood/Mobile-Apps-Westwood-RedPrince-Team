7be61784d0bbe9ba516ffcc35cd4a32f0e835100
Blairqiao <yanzhe.qiao.1@gmail.com>
2026-04-16 15:30:34 -0500

    titles

---
+++ RedPrince/Models/Titles/HomePageTitles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedPrince.Models.Titles
+{
+    public class HomePageTitles
+    {
+
+        public static string Title { get; } = "Home Page";
+        public static string GamesButton { get; } = "Games";
+        public static string LeaderboardButton { get; } = "Leaderboard";
+        public static string StoreButton { get; } = "Store";
+        public static string SettingsButton { get; } = "Settings";
+
+
+    }
+}
