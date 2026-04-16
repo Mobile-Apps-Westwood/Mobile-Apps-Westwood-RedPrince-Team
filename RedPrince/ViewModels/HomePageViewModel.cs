9d4a02957ce01ee28f2983ea5338a994aad04bfd
Blairqiao <yanzhe.qiao.1@gmail.com>
2026-04-16 15:27:59 -0500

    HomePage

--- RedPrince/ViewModels/HomePageViewModel.cs
+++ RedPrince/ViewModels/HomePageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,7 +9,37 @@ namespace RedPrince.ViewModels
     public partial class HomePageViewModel : ObservableObject
     {
 
+        //public string Title => ;
+        //public string Collection => ;
+        //public string CollectionImage => ;
+        //public string CollectionButton => ;
 
+        [RelayCommand]
+        private async Task GamesButtonClicked()
+        {
+            await Shell.Current.GoToAsync(nameof());
+        }
+
+        [RelayCommand]
+        private async Task LeaderBoardsButtonClicked()
+        {
+            await Shell.Current.GoToAsync(nameof());
+        }
+
+        [RelayCommand]
+        private async Task StoreButtonClicked()
+        {
+            await Shell.Current.GoToAsync(nameof());
+        }
+        [RelayCommand]
+        private async Task SettingsButtonClicked()
+        {
+            await Shell.Current.GoToAsync(nameof());
+        }
+
+        public HomePageViewModel()
+        {
+        }
 
     }
 }
