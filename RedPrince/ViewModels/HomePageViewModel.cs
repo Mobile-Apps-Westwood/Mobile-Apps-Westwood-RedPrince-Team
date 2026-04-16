f1f4140197f3649012b619cb4587d8a1f50ed634
Blairqiao <yanzhe.qiao.1@gmail.com>
2026-04-16 15:20:51 -0500

    homepage

---
+++ RedPrince/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RedPrince.ViewModels
+{
+    public partial class HomePageViewModel : ObservableObject
+    {
+
+
+
+    }
+}
---
+++ RedPrince/Views/HomePageView.xaml.cs
@@ -0,0 +1,9 @@
+namespace RedPrince.Views;
+
+public partial class HomePageView : ContentView
+{
+	public HomePageView()
+	{
+		InitializeComponent();
+	}
+}
